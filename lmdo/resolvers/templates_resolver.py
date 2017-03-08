c2ca2932ba6aa29e002317ce7775611a2ed39d42
Roger Liang <roger@merlingroup.co>
2017-03-08 05:58:34 +0000

    fix template resovler for yaml file

--- lmdo/resolvers/templates_resolver.py
+++ lmdo/resolvers/templates_resolver.py
@@ -36,10 +36,8 @@ class TemplatesResolver(Resolver):
             "children": []
         }
 
-        templates['master'] = self._template_path
-        with open(templates['master'], 'r') as outfile:
-            master_tpl = json.loads(outfile.read())
-
+        master_tpl = FileLoader(file_path=self._template_path, allowed_ext=FILE_LOADER_TEMPLATE_ALLOWED_EXT).process()
+        
         template_urls = []
         for name, resource in master_tpl['Resources'].iteritems():
             if resource['Type'] == 'AWS::CloudFormation::Stack':
@@ -50,6 +48,8 @@ class TemplatesResolver(Resolver):
                 if url.startswith('$template'):
                     header, template_name = url.split("|")
                     templates['children'].append(self.create_template(template_name))
+        
+        templates['master'] = self.create_template(self._template_path)
 
         return templates
 
@@ -62,8 +62,12 @@ class TemplatesResolver(Resolver):
 
         env_var_convertor.successor = stack_var_convertor
         stack_var_convertor.successor = nested_template_convertor
+         
+        if not os.path.isfile(template_name):
+            file_path = self.find_template(template_name)
+        else: 
+            file_path = template_name
 
-        file_path = self.find_template(template_name)
         if not file_path:
             Oprint.err('Cannot find template {} in {}'.format(template_name, self._repo_path))
 
@@ -85,7 +89,6 @@ class TemplatesResolver(Resolver):
         if os.path.isdir(self._repo_path):
             findings = FileLoader.find_files_by_names(search_path=self._repo_path, only_files=[template_name])
        
-        print(findings)
         # Only return the first found
         return findings[0] if findings else None
 
